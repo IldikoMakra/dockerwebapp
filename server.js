bbe5c341bc394c00b9eb84ebb2fe3fafa3e76a09
Ildiko Makra <70936723+IldikoMakra@users.noreply.github.com>
2023-01-16 14:20:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Ildiko Makra Server v0.2");
+  res.send("Ildiko Makra Server v0.3");
 });
 
 app.listen(PORT, HOST, () => {
